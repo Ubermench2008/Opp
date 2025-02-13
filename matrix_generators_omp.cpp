2957a2d19c476843fd99235aa1db7b6c564db7b2
Ubermench2008 <i.danilov@g.nsu.ru>
2025-02-13 21:00:58 +0700

    Update matrix_generators_omp.cpp

--- matrix_generators_omp.cpp
+++ matrix_generators_omp.cpp
@@ -1,17 +1,10 @@
-// matrix_generators.cpp:
+
 #include "matrix_generators.h"
 #include <cmath>
-#include <omp.h>   // Подключаем OpenMP
+#include <omp.h>
 
-// Если константа M_PI не определена, определим её.
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-// Функция генерации матрицы A и вектора b для режима 2
 void generateMatrix(int N, vector<vector<double>> &A, vector<double> &b) {
     A.assign(N, vector<double>(N, 1.0));
-    // Параллельное заполнение диагонали
     #pragma omp parallel for schedule(auto)
     for (int i = 0; i < N; ++i) {
         A[i][i] = 2.0;
@@ -19,21 +12,17 @@ void generateMatrix(int N, vector<vector<double>> &A, vector<double> &b) {
     b.assign(N, N + 1.0);
 }
 
-// Функция генерации матрицы A, модельного решения u и вычисление b = A * u
 void generateMatrixModel(int N, vector<vector<double>> &A, vector<double> &b, vector<double> &u) {
-    // Генерируем матрицу A
     A.assign(N, vector<double>(N, 1.0));
     #pragma omp parallel for schedule(auto)
     for (int i = 0; i < N; ++i) {
         A[i][i] = 2.0;
     }
-    // Формируем вектор u
     u.resize(N);
     #pragma omp parallel for schedule(auto)
     for (int i = 0; i < N; ++i) {
         u[i] = sin(2 * M_PI * i / N);
     }
-    // Вычисляем b = A * u
     b.assign(N, 0.0);
     #pragma omp parallel for schedule(auto)
     for (int i = 0; i < N; ++i) {
@@ -45,9 +34,7 @@ void generateMatrixModel(int N, vector<vector<double>> &A, vector<double> &b, ve
     }
 }
 
-// Функция для подбора tau
 double suggestTau(int N, double scale) {
-    if (N <= 0) return 0.01; // на всякий случай
     double lamMax = static_cast<double>(N) + 1.0;
     return scale * 2.0 / lamMax;
 }
